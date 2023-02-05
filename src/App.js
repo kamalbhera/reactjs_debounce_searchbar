bc266fb5d5516dcf55f66b2bc99ac074a8a801ca
kamlesh <kamlesh.kumawat@axa-abs-co.in>
2023-02-05 10:51:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState, useCallback } from 'react';
 function App() {
+  const [search, setSearch ] = useState([]);
+  const debounce = (func) => {
+    let timer;
+    return function (...args) {
+      const context = this;
+      if(timer) clearTimeout(timer)
+      timer = setTimeout(() => {
+        timer = null
+        func.apply(context, args);
+      }, 500)
+    }
+  }
+  const handleChange = (event) => {
+    const {value} = event.target;
+    fetch(`https://demo.dataverse.org/api/search?q=${value}`)
+    .then(res => res.json())
+    .then(json=> setSearch(json.data.items));
+  }
+  const optimisedVersion = useCallback(debounce(handleChange),[]);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type={'text'} name={'search'} placeholder={'Enter Something...'}
+      className={'search'} onChange={optimisedVersion} /> 
+      {search?.length > 0 && 
+      <div className={'autocomplete'}>
+        {search?.map((el, item) => 
+        <div key={item} className={'autocompleteItems'}>
+              <span>
+                {el.name}
+              </span>
+        </div>
+        )}
+      </div>
+      }
       </header>
     </div>
   );
